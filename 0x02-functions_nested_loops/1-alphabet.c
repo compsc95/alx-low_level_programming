192503a2ceb5dfaecb1e2441ec15bb68c602431b
compsc95 <kibetreagan@gmail.com>
2023-02-21 15:28:04 +0300

    Alpabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,17 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints the letter of the alphabet
+ * print_alphabet - prints the letters of the alphabet
  *
  * Return: 0 Always
  */
 
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char alph;
 
 	for (alph = 'a'; alph <= 'z'; alph++)
 	{
-	_putchar(alph);
+		_putchar(alph);
 	}
 	_putchar('\n');
-
-}
