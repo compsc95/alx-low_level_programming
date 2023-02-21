c6f5e08a0c9b35b92dfbb5be2b1e880c62d4a68e
compsc95 <kibetreagan@gmail.com>
2023-02-21 16:06:06 +0300

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,14 +7,14 @@
  * Return: 1 or 0 or -1
  */
 
-int print_sign(int d)
+int print_sign(int n)
 {
-	if (d > 0)
+	if (n > 0)
 	{
 		_putchar ('+');
 		return (1);
 	}
-	else if (d == 0)
+	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
