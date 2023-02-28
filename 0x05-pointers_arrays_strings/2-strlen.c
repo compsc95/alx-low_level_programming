982fa2f51ef2bbd96241785de52490b4af148485
compsc95 <kibetreagan@gmail.com>
2023-02-28 12:41:14 +0300

    Srtle

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer
+ * Return: Nothing
+ */
+
+int _strlen(char *s)
+{
+	int c;
+
+	c = 0;
+
+	for (c = 0; s[c] != 0; c++)
+	{
+
+	}
+	return (c);
+}
